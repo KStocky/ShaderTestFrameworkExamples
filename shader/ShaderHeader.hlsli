1c75a6a1b498083565181fa646495c0acc8e9f05
KStocky <kstocky1@gmail.com>
2024-11-30 16:23:19 +0000

    Fixing up examples after root signature generation has been automated.

--- shader/ShaderHeader.hlsli
+++ shader/ShaderHeader.hlsli
@@ -1,7 +1,10 @@
-
+#ifndef SHADER_HEADER
+#define SHADER_HEADER
 
 template<typename T>
 T Identity(T In)
 {
     return In;
 }
+
+#endif
--- shader/ShaderTest.hlsl
+++ shader/ShaderTest.hlsl
@@ -2,16 +2,14 @@
 
 #include "/Test/STF/ShaderTestFramework.hlsli"
 
-[RootSignature(SHADER_TEST_RS)]
 [numthreads(1, 1, 1)]
-void ThisTestShouldPass(uint3 DispatchThreadId : SV_DispatchThreadID)
+void ThisTestShouldPass()
 {
     ASSERT(AreEqual, Identity(42), 42);
 }
                         
-[RootSignature(SHADER_TEST_RS)]
 [numthreads(1, 1, 1)]
-void ThisTestShouldFail(uint3 DispatchThreadId : SV_DispatchThreadID)
+void ThisTestShouldFail()
 {
     ASSERT(Fail);
 }
